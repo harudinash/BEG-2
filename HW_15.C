c56e174a2b944b1fe0ac18420c0f6d2f16f78ada
harudinash <46379892+harudinash@users.noreply.github.com>
2019-01-28 23:56:29 +0530

    Update HW_15.C

--- HW_15.C
+++ HW_15.C
@@ -5,6 +5,10 @@ void main()
  printf("INPUT :");
  scanf("%d",&a);
  printf("OUTPUT :\n");
+	if(a==0)
+		printf("invalid");
+	else{
+		
 	 c=a;d=a;e=a;l=0;p=1;x=0;
 	 while(a)
 	 {
@@ -35,6 +39,6 @@ void main()
 	    {
 	    printf("no");
 	    }
- 
+	}
  getch();
 }
