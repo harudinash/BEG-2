fb7ba6f97b51767c6ba40600443e1eb354470ab2
harudinash <46379892+harudinash@users.noreply.github.com>
2019-01-28 23:52:10 +0530

    Update HW_15.C

--- HW_15.C
+++ HW_15.C
@@ -2,17 +2,16 @@
 void main()
 {
  int a,b,c,d,e,g,x,r,v,p=1,l=0;
- clrscr();
- scanf("%d",&g);
+ 
+ 
 
- while(g)
- {
+ printf("INPUT :");
  scanf("%d",&a);
- printf("\n1.Amstrong\n2.Pallindrome");
- scanf("%d",&b);
- switch (b)
-   {
-     case 1:
+ printf("OUTPUT :\n");
+ 
+ 
+  
+     
 	 c=a;d=a;e=a;l=0;p=1;x=0;
 	 while(a)
 	 {
@@ -29,46 +28,23 @@ void main()
 	     {
 	      p=p*r;
 	      l--;
-	    printf("p=%d",p);
+	    
 	    }
 	     x=x+p;
 	     c=c/10;
-	     printf("x=%d",x);
+	     
 	     }
 	     if(x==d)
 	     {
-	    printf("amstrong");
+	    printf("yes");
 	    }
 	    else
 	    {
-	    printf("not amstrong");
+	    printf("no");
 	    }
-	    break;
+	   
 
-	 case 2:
-		   c=a;d=0;e=a;r=0;
-	 while(a)
-	 {
-	  a=a/10;
-	  l++;
-	  }
-	  while(c)
-	    {
-	     r=c%10;
-	     d=d*10+r;
-	     c=c/10;
-	     }
-	  if(d==e)
-	  {
-	   printf("pallindrome");
-	   }
-	  else
-	  {
-	  printf("not pallindrome");
-	  }
-	    break;
-    }
-  g--;
-    }
+	 
+    
  getch();
 }
