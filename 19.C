9f2c77a4cc50a8100821f5f42fd06b370c303a1c
harudinash <46379892+harudinash@users.noreply.github.com>
2019-01-29 00:23:32 +0530

    Create 19.C

---
+++ 19.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+   
+int i,t=1,a;
+char b[1];
+printf("input :");
+
+scanf("%d",&a);
+printf("output :\n");
+for(i=1;i<6;i++)
+{
+t=a*i;
+printf("%d ",t);
+
+}
+
+    return 0;
+}
